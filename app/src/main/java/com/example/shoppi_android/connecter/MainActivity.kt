0e9cafd4fef2c8fe2f3d0e9e0d9eaea700bd491e
mac <gnl96@naver.com>
2022-08-18 14:09:23 +0900

    첫 실행 test

---
+++ app/src/main/java/com/example/shoppi_android/connecter/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.shoppi_android.connecter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("깃허브연")
+        Log.d("asdf","asdf")
+
+
+    }
+}
